21425791a79fd86b628814d05e3112ae463ff00c
Frame <dave@macrae.org.uk>
2021-01-05 13:38:14 +0000

    Version 1.0
    Still a few things to do around setting up arguments as per TODO list.

--- quickstart.py
+++ quickstart.py
@@ -2,7 +2,8 @@
 
 from __future__ import print_function
 import pickle
-import os.path
+import os
+import sys
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
@@ -23,11 +24,20 @@ def main():
     archive message by removing label "INBOX"
     """
 
+    # TODO: What is the correct error handling here
     service = build('gmail', 'v1', credentials=login())
-    want = get_labels(service, want='frame')
-    # need to check if "want" was filled
+    labelID = get_labels(service, labelName='frame')
 
-    get_messages(service, want)
+    messages = get_messages(service, labelID)
+
+    if not messages:
+        print('No unread messages found.')
+    else:
+        print('Found', len(messages), 'Unread Message(s)')
+        for message in messages:
+            get_message_content(service, message['id'])
+            mark_message_read(service, message['id'])
+            pass
 
 
 def login():
@@ -54,33 +64,53 @@ def login():
     return gmail_creds
 
 
-def get_labels(service, want=None):
+def get_labels(service, labelName=None):
     # Call the Gmail API
 
-    # If we want to get the ID of a specific label, pass this through in the 'want' parameter.
+    """
+    We want to get a list of all labels that are defined in our GMAIL account.
+    When we get the list this includes the name of the label and the ID. It is the ID that we need for subsequent
+    calls to the API.
 
-    results = service.users().labels().list(userId='me').execute()
-    labels = results.get('labels', [])
+    :param service: Authorized Gmail API service instance.
+    :param labelName: the label that tags the messages we're interested in
 
-    wantId = ''
+    The function returns the ID of the required label.
+
+    """
+    results, labels, labelId = None, None, None
+
+    try:
+        results = service.users().labels().list(userId='me').execute()
+        labels = results.get('labels', [])
+    except errors.HttpError as error:   # this is probably not the best solution
+        print('An error occurred: %s' % error)
 
     if not labels:
         print('No labels found.')
     else:
         for label in labels:
-            if label['name'] == want:
-                wantId = label['id']
+            if label['name'] == labelName:
+                labelId = label['id']
 
-    return wantId
+    return labelId
 
 
 def get_messages(service, label=None):
-    # Call the Gmail API
-
     """
     We want to get a list of all messages (defined by a specific label if required)
     We should also only get unread messages.
+
+    :param service: Authorized Gmail API service instance.
+    :param label: the label that tags the messages we're interested in
+
     """
+
+    # Even if label is not specified, we want to only retrieve UNREAD messages
+    # If "label" is not defined then we will retrieve all unread messages from the mailbox.
+    # TODO: need to determine if we want to retrieve all UNREAD messages (which is the current behavior) or just
+    #  those that haven't been archived.
+    #  i.e. do we need to make INBOX part of the label list.
 
     if label is None:
         label = ['UNREAD']
@@ -88,32 +118,48 @@ def get_messages(service, label=None):
         label = [label, 'UNREAD']
 
     messages = []
-    results = service.users().messages().list(userId='me', labelIds=label, pageToken=None).execute()
-    messages.extend(results.get('messages', []))
 
-    while results.get('nextPageToken'):
-        results = service.users().messages().list(userId='me', labelIds=label, pageToken=results.get('nextPageToken')).execute()
+    try:
+        """
+            Loop through the messages in the GMAIL inbox. 
+            Gmail returns a maximum of 100 messages in a call with nextPageToken set to indicate
+            that there are more messages.
+            
+            The call that retrieves that last block will not have this set.
+        """
+        # get the initial set of messages (if any)
+        results = service.\
+            users().\
+            messages().\
+            list(userId='me', labelIds=label, pageToken=None).execute()
         messages.extend(results.get('messages', []))
 
-    if not messages:
-        print('No unread messages found.')
-    else:
-        print('Found', len(messages), 'Unread Message(s)')
-        for message in messages:
-            # get_message_content(service, message['id'])
-            # mark_message_read(service, message['id'])
-            pass
-    pass
+        while results.get('nextPageToken'):
+            # if nextPageToken is set then there are more messages to retrieve.
+            results = service.\
+                users().\
+                messages().\
+                list(userId='me', labelIds=label, pageToken=results.get('nextPageToken')).execute()
+            # extend the message list to include the new ones retrieved.
+            messages.extend(results.get('messages', []))
+
+    except errors.HttpError as error:   # this is probably not the best solution
+        print('An error occurred: %s' % error)
+
+    return messages
 
 
 def get_message_content(service, msg_id):
-    """Get and store attachment from Message with given id.
+    """
+    Get and store attachment from Message with given id.
 
     :param service: Authorized Gmail API service instance.
     :param msg_id: ID of Message containing attachment.
     """
 
     uid = 'me'
+    # TODO: Should probably make this an arg
+    outputDir = 'output/'
 
     try:
         message = service.users().messages().get(userId=uid, id=msg_id).execute()
@@ -130,27 +176,52 @@ def get_message_content(service, msg_id):
                         attachments().get(userId=uid, messageId=msg_id, id=att_id).execute()
                     data = att['data']
                 file_data = base64.urlsafe_b64decode(data.encode('UTF-8'))
-                path = part['filename']
+
+                # it appears that filename can contain non-safe characters like "/", i.e. it has a full path
+                # this appears to be because the UNIX mail command includes the full path of the attachment as given:
+                #   mail -A ~/PycharmProjects/frame/ACF1083.jpg frame@macrae.org.uk
+                # we should do a basename on the filename to just get the bit we need.
+
+                path = outputDir + os.path.basename(part['filename'])
                 print("Save File:", path)
-                with open(path, 'wb') as f:
-                    f.write(file_data)
+                try:
+                    with open(path, 'wb') as f:
+                        f.write(file_data)
+                except OSError as err:
+                    print("OS error: {0}".format(err))
+                    sys.exit(1)
+                except Exception:
+                    print("Unexpected error:", sys.exc_info()[0])
+                    raise
 
     except errors.HttpError as error:
         print('An error occurred: %s' % error)
 
 
 def mark_message_read(service, msg_id):
-    # We now need to mark the message as read.
+    """
+    We now need to mark the message as read.
+    This is done by removing the UNREAD label
+    The message is also archived by removing the INBOX label.
 
+    :param service: Authorized Gmail API service instance.
+    :param msg_id: ID of message to be altered
+    """
     uid = 'me'
+    message = None
 
-    message = service. \
-        users(). \
-        messages(). \
-        modify(userId=uid, id=msg_id, body={'removeLabelIds': ['UNREAD', 'INBOX']}).execute()
+    try:
+        message = service. \
+            users(). \
+            messages(). \
+            modify(userId=uid, id=msg_id, body={'removeLabelIds': ['UNREAD', 'INBOX']}).execute()
+    except errors.HttpError as error:
+        print('An error occurred: %s' % error)
+
     # need to check that the UNREAD label has been removed.
 
-    if 'UNREAD' in message['labelIds']:
+    labelIDs = message.get('labelIds')
+    if 'UNREAD' in labelIDs:
         print("Something went wrong, label UNREAD still there")
     else:
         print("Message", msg_id, "marked as Read")
@@ -159,4 +230,17 @@ def mark_message_read(service, msg_id):
 
 
 if __name__ == '__main__':
-    main()
+    # TODO: Need to have some kind of argument parsing
+    #   output folder
+    #   verbosity level
+    #   do we want to limit the number of files
+    #   do we want to overwrite existing files
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(1)
+        except SystemExit:
+            # noinspection PyProtectedMember
+            os._exit(1)
